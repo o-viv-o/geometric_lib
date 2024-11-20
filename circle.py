7bfc64d1448cb52bcf800516b986f18ec6736f83
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 01:10:00 +0300

    Updated circle.py conditions

--- circle.py
+++ circle.py
@@ -1,10 +1,14 @@
 import math
 
-
 def area(r):
-    return math.pi * r * r
+    if r >= 0:
+        return math.pi * r * r
+    else:
+        raise ValueError("Radius must be greater than zero.")
 
 
 def perimeter(r):
-    return 2 * math.pi * r
-
+    if r >= 0:
+        return 2 * math.pi * r
+    else:
+        raise ValueError("Radius must be greater than zero.")
