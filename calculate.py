5c62f6de494e5d00b6e04a403722f9693f8ff1c2
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 02:33:57 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -5,43 +5,48 @@ import triangle
 figs = ["circle", "square", "triangle"]
 funcs = ["perimeter", "area"]
 sizes = {
-    "perimeter-circle" : 1,
-    "area-circle" : 1,
-    "perimeter-square" : 1,
-    "area-square" : 1,
-    "perimeter-triangle" : 3,
-    "area-triangle" : 3,
+    "perimeter-circle": 1,
+    "area-circle": 1,
+    "perimeter-square": 1,
+    "area-square": 1,
+    "perimeter-triangle": 3,
+    "area-triangle": 3,
 }
 
 
-def calc(fig, func, size) :
+def calc(fig, func, size):
     assert fig in figs, f"Figure '{fig}' not recognized. Available figures: {figs}"
     assert func in funcs, f"Function '{func}' not recognized. Available functions: {funcs}"
 
-    result = eval( f"{fig}.{func}(*{size})" )
+    result = eval(f"{fig}.{func}(*{size})")
     return result
 
 
-def main() :
+def main():
+    tmp1 = square.area(4)
+    tmp2 = triangle.area(2, 3, 4)
+    tmp3 = circle.area(6)
+
     func = ""
     fig = ""
     size = []
 
-    while fig not in figs :
-        fig = input(f"Enter figure name, available are {figs} : \n" )
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {figs}:\n")
 
-    while func not in funcs :
-        func = input(f"Enter function name, available are {funcs} : \n" )
+    while func not in funcs:
+        func = input(f"Enter function name, available are {funcs}:\n")
 
     if fig == "circle" or fig == "square":
-        while len(size) != sizes.get(f"{func}-{fig}", 1) :
-            size = list(map (int, input("Input figure sizes separated by space. Sizes need to be > 0\n"). split(" "), ))
+        while len(size) != sizes.get(f"{func}-{fig}", 1):
+            size = list(map(int,input("Input figure sizes separated by space. Sizes need to be > 0\n").split(" "),))
     else:
-        while len(size) != sizes.get(f"{func}-{fig}", 3) :
-            size = list(map(int, input("Input figure sizes separated by space. Sizes need to be > 0\n"). split(" "), ))
+        while len(size) != sizes.get(f"{func}-{fig}", 3):
+            size = list(map(int,input( "Input figure sizes separated by space. Sizes need to be > 0\n").split(" "),))
+
     res = calc(fig, func, size)
     print(res)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
