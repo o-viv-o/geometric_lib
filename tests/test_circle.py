cf166b18f75287d9073ab044a6666e3e2584a11b
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 16:17:04 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -2,16 +2,16 @@ import unittest
 import calculate
 
 
-class TestCircle(unittest.TestCase):
+class TestCircle(unittest.TestCase): 
     """Area tests."""
 
-    def test_area_positive(self):
+    def test_area_positive(self): 
         self.assertEqual(calculate.calc("circle", "area", [1]),
                          3.141592653589793)
         self.assertEqual(calculate.calc("circle", "area", [3]),
                          28.274333882308138)
 
-    def test_area_negative(self):
+    def test_area_negative(self): 
         error_message = "Radius must be > 0."
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "area", [-1])
@@ -23,19 +23,19 @@ class TestCircle(unittest.TestCase):
 
     """Perimeter tests."""
 
-    def test_perimeter_positive(self):
+    def test_perimeter_positive(self): 
         self.assertEqual(calculate.calc("circle", "perimeter", [1]),
                          6.283185307179586)
         self.assertEqual(calculate.calc("circle", "perimeter", [6]),
                          37.69911184307752)
 
-    def test_perimeter_negative(self):
+    def test_perimeter_negative(self): 
         error_message = "Radius must be > 0."
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "perimeter", [-1])
         self.assertEqual(str(context.exception), error_message)
 
-        with self.assertRaises(ValueError) as context:
+        with self.assertRaises(ValueError) as context: 
             calculate.calc("circle", "perimeter", [-6])
         self.assertEqual(str(context.exception), error_message)
 
