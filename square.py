679c98cc0425c19ec19796a0a1ef880722c83232
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 13:56:36 +0300

    Update square.py

--- square.py
+++ square.py
@@ -2,11 +2,11 @@ def area(a):
     if a >= 0:
         return a * a
     else:
-        raise ValueError("Size must be greater than zero.")
+        raise ValueError("Size must be > 0.")
 
 
 def perimeter(a):
     if a >= 0:
         return 4 * a
     else:
-        raise ValueError("Size must be greater than zero.")
+        raise ValueError("Size must be > 0.")
