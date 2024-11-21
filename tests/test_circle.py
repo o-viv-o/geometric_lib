e1db1a936d574a476c3813350664de2d504f30de
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 13:52:13 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -10,8 +10,10 @@ class TestCircle(unittest.TestCase):
         self.assertEqual(res_area_zero, 0)
 
     def test_area_positive(self):
-        self.assertEqual(calculate.calc("circle", "area", [1]), 3.141592653589793)
-        self.assertEqual(calculate.calc("circle", "area", [3]), 28.274333882308138)
+        self.assertEqual(calculate.calc("circle", "area", [1]),
+                         3.141592653589793)
+        self.assertEqual(calculate.calc("circle", "area", [3]),
+                         28.274333882308138)
 
     def test_area_negative(self):
         with self.assertRaises(ValueError) as context:
@@ -29,8 +31,10 @@ class TestCircle(unittest.TestCase):
         self.assertEqual(res_perimeter_zero, 0)
 
     def test_perimeter_positive(self):
-        self.assertEqual(calculate.calc("circle", "perimeter", [1]), 6.283185307179586)
-        self.assertEqual(calculate.calc("circle", "perimeter", [6]), 37.699111843077516)
+        self.assertEqual(calculate.calc("circle", "perimeter", [1]),
+                         6.283185307179586)
+        self.assertEqual(calculate.calc("circle", "perimeter", [6]),
+                         37.699111843077516)
 
     def test_perimeter_negative(self):
         with self.assertRaises(ValueError) as context:
