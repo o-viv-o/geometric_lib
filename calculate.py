7ab85164ec3639d7ecca8a1f87cd22fabeb94aba
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 16:21:44 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -30,12 +30,13 @@ if __name__ == "__main__":
     size = list()
 
     while fig not in figs:
-        fig = input(f"Enter figure name, available are {figs}:\n")
+        fig = input(f"Enter figure name, available are {figs}: \n")
 
     while func not in funcs:
-        func = input(f"Enter function name, available are {funcs}:\n")
+        func = input(f"Enter function name, available are {funcs}: \n")
 
     while len(size) != sizes.get(f"{func}-{fig}", 1):
-        size = list(map(int, input("Input figure sizes separated by space.\n").split(' ')))
+        size = list(map(int, input("Input figure sizes"
+        "separated by space.\n").split(' ')))
 
     print(calc(fig, func, size))
