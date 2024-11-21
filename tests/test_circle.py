4997758a0ed53ac9ec6f76d356c851b7388d6c9a
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 13:58:44 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -10,10 +10,8 @@ class TestCircle(unittest.TestCase):
         self.assertEqual(res_area_zero, 0)
 
     def test_area_positive(self):
-        self.assertEqual(calculate.calc("circle", "area", [1]),
-                         3.141592653589793)
-        self.assertEqual(calculate.calc("circle", "area", [3]),
-                         28.274333882308138)
+        self.assertEqual(calculate.calc("circle", "area", [1]), 3.14159)
+        self.assertEqual(calculate.calc("circle", "area", [3]), 28.27433)
 
     def test_area_negative(self):
         with self.assertRaises(ValueError) as context:
