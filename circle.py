5700a064396ac3bab85d2f9914fbe9dee86b81e8
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 14:33:39 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -5,7 +5,7 @@ def area(r):
     if r >= 0:
         return math.pi * r * r
     else:
-        raise ValueError("Radius must be > 0."")
+        raise ValueError("Radius must be > 0.")
 
 
 def perimeter(r):
