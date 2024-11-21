2a307a10537f34784eb59a1cf11a84d14b2dc7fc
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 13:05:21 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -2,6 +2,7 @@ import circle
 import triangle
 import square
 
+
 figs = ["circle", "square", "triangle"]
 funcs = ["perimeter", "area"]
 sizes = {
@@ -13,6 +14,7 @@ sizes = {
     "area-triangle": 3,
 }
 
+
 def calc(fig, func, size):
     assert fig in figs
     assert func in funcs
@@ -20,21 +22,26 @@ def calc(fig, func, size):
     result = eval(f"{fig}.{func}(*{size})")
     return result
 
+
 if __name__ == "__main__": 
     tmp1 = square.area(4)
     tmp2 = triangle.area(2, 3, 4)
     tmp3 = circle.area(6)
 
+    
     func = ""
     fig = ""
     size = list()
 
+    
     while fig not in figs:
         fig = input(f"Enter figure name, available are {figs}: \n")
 
+    
     while func not in funcs:
         func = input(f"Enter function name, available are {funcs}: \n")
-        
+
+    
     if fig == "circle" or fig == "square":
         while len(size) != sizes.get(f"{func}-{fig}", 1):
             size = list(
