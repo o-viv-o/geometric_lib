05a106911a2e2783e09e3133f014af0ed11335ae
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 13:56:52 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -2,11 +2,11 @@ def area(a, b, c):
     if a >= 0 and b >= 0 and c >= 0:
         return (a + b + c) / 2
     else:
-        raise ValueError("Size must be greater than zero.")
+        raise ValueError("Size must be > 0.")
 
 
 def perimeter(a, b, c):
     if a >= 0 and b >= 0 and c >= 0:
         return a + b + c
     else:
-        raise ValueError("Size must be greater than zero.")
+        raise ValueError("Size must be > 0.")
