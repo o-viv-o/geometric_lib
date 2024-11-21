744e4649fa61fcde78fc243b5a40925b0390f444
Viktoria Obukhova <vikaenimle@gmail.com>
2024-11-21 14:32:07 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,17 +1,18 @@
 import unittest
 import calculate
+import math
+from circle import area, perimeter
+
 
 class TestCircle(unittest.TestCase):
-    """Area tests."""
 
-    def test_area_zero(self):
-        res_area_zero = (calculate.calc("circle", "area", [0]), 0)
+    def test_area_positive_radius(self):
+        self.assertAlmostEqual(area(5), math.pi * 25, places=5)
 
-    def test_area_positive(self):
-        self.assertEqual(calculate.calc("circle", "area", [1]), 3.141592653589793)
-        self.assertEqual(calculate.calc("circle", "area", [3]), 28.274333882308138 )
+    def test_perimeter_positive_radius(self):
+        self.assertAlmostEqual(perimeter(5), 2 * math.pi * 5, places=5)
 
-    def test_area_negative(self):
+   def test_area_negative(self):
         error_message = "Radius must be > 0."
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "area", [-1])
@@ -20,18 +21,8 @@ class TestCircle(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "area", [-6])
         self.assertEqual(str(context.exception), error_message)
-
-    """Perimeter tests."""
-
-    def test_perimeter_zero(self):
-        res_perimeter_zero = calculate.calc("circle", "perimeter", [0])
-        self.assertEqual(res_perimeter_zero, 0)
-
-    def test_perimeter_positive(self):
-        self.assertEqual(calculate.calc("circle", "perimeter", [1]), 6.283185307179586)
-        self.assertEqual(calculate.calc("circle", "perimeter", [6]), 37.699111843077516)
-
-    def test_perimeter_negative(self):
+       
+   def test_perimeter_negative(self):
         error_message = "Radius must be > 0."
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "perimeter", [-1])
@@ -40,7 +31,8 @@ class TestCircle(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "perimeter", [-6])
         self.assertEqual(str(context.exception), error_message)
+   
 
 
-if __name__ == "__main__": 
+if __name__ == '__main__':
     unittest.main()
